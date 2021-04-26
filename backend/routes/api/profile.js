40e8801fffc89d3543f979b1f0eaa73b21ceb833
Yevhenii <yorieshkin@gmail.com>
2021-04-26 21:24:56 +0300

    add profile experience

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -36,8 +36,8 @@ router.post(
   [
     auth,
     [
-      check('status', 'Status is required').not().isEmpty(),
-      check('skills', 'Skills is required').not().isEmpty(),
+      check('status', 'Status is required').notEmpty(),
+      check('skills', 'Skills is required').notEmpty(),
     ],
   ],
   async (req, res) => {
@@ -160,5 +160,59 @@ router.delete('/', auth, async (req, res) => {
     res.status(500).send('Server error')
   }
 })
+
+// @desc Add profile experience
+// @route PUT /api/profile/experience
+// @access Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required').notEmpty(),
+      check('company', 'Company is required').notEmpty(),
+      check('from', 'From date is required').notEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    }
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id })
+
+      profile.experience.unshift(newExp)
+
+      await profile.save()
+
+      res.json(profile)
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send('Server Error')
+    }
+  }
+)
 
 module.exports = router
