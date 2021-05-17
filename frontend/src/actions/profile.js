59b0251cce92fbffb786e4190d2987a017a254c7
Yevhenii <yorieshkin@gmail.com>
2021-05-17 20:40:49 +0300

    add CreateProfile component & action

--- frontend/src/actions/profile.js
+++ frontend/src/actions/profile.js
@@ -4,7 +4,7 @@ import { setAlert } from './alert'
 import { GET_PROFILE, PROFILE_ERROR } from './types'
 
 //Get current user profile
-const getCurrentProfile = () => async (dispatch) => {
+export const getCurrentProfile = () => async (dispatch) => {
   try {
     const res = await axios.get('/api/profile/me')
 
@@ -20,4 +20,35 @@ const getCurrentProfile = () => async (dispatch) => {
   }
 }
 
-export default getCurrentProfile
+// Create or update profile
+export const createProfile =
+  (history, edit = false) =>
+  async (dispatch) => {
+    try {
+      const res = await axios.get('/profile')
+
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      })
+
+      dispatch(
+        setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success')
+      )
+
+      if (!edit) {
+        history.push('/dashboard')
+      }
+    } catch (err) {
+      const errors = err.response.data.errors
+
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')))
+      }
+
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status },
+      })
+    }
+  }
