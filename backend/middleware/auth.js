4290fe52673496925dab9e5bff1df937c92bd842
Yevhenii <yorieshkin@gmail.com>
2021-06-08 20:53:10 +0300

    update auth.js, clean up exports

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,16 +7,21 @@ module.exports = function (req, res, next) {
 
   // Check if not token
   if (!token) {
-    return res.status(401).json({ msg: 'No tiken, authorization denied' })
+    return res.status(401).json({ msg: 'No token, authorization denied' })
   }
 
   // Verify token
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-    req.user = decoded.user
-    next()
-  } catch (error) {
-    res.status(401).json({ msg: 'Token is not valid' })
+    jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ msg: 'Token is not valid' })
+      } else {
+        req.user = decoded.user
+        next()
+      }
+    })
+  } catch (err) {
+    console.error('something wrong with auth middleware')
+    res.status(500).json({ msg: 'Server Error' })
   }
 }
