25dc352f503eb3eac184c2d495bc557234078f39
Yevhenii <yorieshkin@gmail.com>
2021-06-10 21:10:17 +0300

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const colors = require('colors')
 const connectDB = require('./config/db.js')
+const path = require('path')
 
 dotenv.config()
 
@@ -10,15 +11,23 @@ const app = express()
 connectDB()
 
 // Init Middleware
-app.use(express.json({ extended: false }))
-
-app.get('/', (req, res) => res.send('API Running'))
+app.use(express.json())
 
 //Define routes
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
+  })
+} else {
+  app.get('/', (req, res) => res.send('API Running'))
+}
 
 const PORT = process.env.PORT || 5000
 
